f97d5472e0a2c2308ff43f381911662f5acca21a
hp3icc <49044505+hp3icc@users.noreply.github.com>
2022-06-23 21:37:18 -0500

    Update update.sh

--- update.sh
+++ update.sh
@@ -29,6 +29,42 @@ exit 0
 
 EOF
 #
+sudo cat > /bin/menu-up-fdm <<- "EOF"
+#!/bin/bash
+while : ; do
+choix=$(whiptail --title "Raspbian Proyect HP3ICC / update FreeDMR" --menu "Nota Importante: debe debe agregar todos sus obp en la opcion numero uno, ( 1-Lista de OBP )antes de iniciar la actualizacion, el proceso de actualizacion borrara por completo la carpeta /opt/FreeDMR, al finalizar la actualizacion el servicio Freedmr se reinniciara automaticamente.
+" 18 55 5 \
+1 " List OBP " \
+2 " List Rules  " \
+3 " shell extra " \
+4 " Iniciar Actualizacion FreeDMR  " \
+5 " Menu Principal " 3>&1 1>&2 2>&3)
+exitstatus=$?
+#on recupere ce choix
+#exitstatus=$?
+if [ $exitstatus = 0 ]; then
+    echo "Your chosen option:" $choix
+else
+    echo "You chose cancel."; break;
+fi
+# case : action en fonction du choix
+case $choix in
+1)
+sudo nano /opt/obp.txt ;;
+2)
+sudo nano /opt/rules.txt ;;
+3)
+sudo nano /opt/extra-1.sh && chmod +x /opt/extra* ;;
+4)
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/hp3icc/emq-TE1ws/main/fdmr-update.sh)" ;;
+5)
+break;
+esac
+done
+exit 0
+EOF
+#
+#
 sudo cat > /bin/menu-up-fdmon <<- "EOF"
 #!/bin/bash
 while : ; do
