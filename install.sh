dc64f69fbff3c31b53da2fede0897216b1137155
Esteban Mackay Q <49044505+hp3icc@users.noreply.github.com>
2022-06-28 12:18:01 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -497,6 +497,9 @@ sudo sed -i "s/test/selfcare/g"  /opt/FreeDMR/hotspot_proxy_v2.py
 sudo sed -i "s/\/freedmr.cfg/\/config\/FreeDMR.cfg/g"  /opt/FreeDMR/hotspot_proxy_v2.py
 sudo sed -i "s/test/selfcare/g"  /opt/FreeDMR/proxy_db.py
 sudo sed -i "s/root/emqte1/g"  /opt/FreeDMR/proxy_db.py
+#
+sudo sed -i "s/root/emqte1/g"  /opt/FreeDMR/proxy.cfg
+sudo sed -i "s/test/selfcare/g"  /opt/FreeDMR/proxy.cfg
 #################
 
 #sed '33 a <!--' -i /var/www/html/sysinfo.php
