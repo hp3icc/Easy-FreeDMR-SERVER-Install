a8bb30fc2172d75864bf9788947ba1e8b065387c
Esteban Mackay Q <49044505+hp3icc@users.noreply.github.com>
2022-12-07 22:52:51 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -102,57 +102,7 @@ sudo chmod +777 /var/log
 sudo chmod +777 /var/log/*
 
 #
-sudo cat > /bin/menu <<- "EOF"
-#!/bin/bash
-while : ; do
-choix=$(whiptail --title "Raspbian Proyect HP3ICC EasyFreeDMR" --menu "move up or down with the keyboard arrows and select your option by pressing enter:" 23 56 13 \
-1 " Edit FreeDMR Server " \
-2 " Edit Interlink  " \
-3 " Edit FDMR-Monitor  " \
-4 " Edit Port HTTP  " \
-5 " Parrot on  " \
-6 " Parrot off  " \
-7 " Restart FreeDMR Server  " \
-8 " Restart FDMR-Monitor " \
-9 " Menu update " 3>&1 1>&2 2>&3)
-exitstatus=$?
-#on recupere ce choix
-#exitstatus=$?
-if [ $exitstatus = 0 ]; then
-    echo "Your chosen option:" $choix
-else
-    echo "You chose cancel."; break;
-fi
-# case : action en fonction du choix
-case $choix in
-1)
-sudo nano /opt/FreeDMR/config/FreeDMR.cfg ;;
-2)
-sudo nano /opt/FreeDMR/config/rules.py ;;
-3)
-sudo nano /opt/FDMR-Monitor/fdmr-mon.cfg ;;
-4)
-sudo nano /etc/apache2/ports.conf && systemctl restart apache2.service ;;
-5)
-sudo systemctl stop fdmrparrot.service && sudo systemctl start fdmrparrot.service && sudo systemctl enable fdmrparrot.service ;;
-6)
-sudo systemctl stop fdmrparrot.service &&  sudo systemctl disable fdmrparrot.service ;;
-7)
-sudo systemctl stop proxy.service && sudo systemctl start proxy.service && sudo systemctl enable proxy.service && sudo systemctl stop freedmr.service && sudo systemctl start freedmr.service && sudo systemctl enable freedmr.service && cronedit.sh '* */6 * * *' 'data-id' add;;
-8)
-sudo systemctl stop fdmr_mon.service && sudo rm /opt/FDMR-Monitor/sysinfo/*.rrd && sh /opt/FDMR-Monitor/sysinfo/rrd-db.sh && cronedit.sh '*/5 * * * *' 'sh /opt/FDMR-Monitor/sysinfo/graph.sh' add && cronedit.sh '*/2 * * * *' 'sh /opt/FDMR-Monitor/sysinfo/cpu.sh' add && sudo systemctl enable fdmr_mon.service && sudo systemctl restart apache2.service && sudo systemctl enable apache2.service && sudo systemctl start fdmr_mon.service;;
-9)
-sh -c "$(curl -fsSL https://github.com/hp3icc/Easy-FreeDMR-SERVER-Install/raw/main/update.sh)";
-esac
-done
-exit 0
-
-
-
-
-EOF
-###
-ln -s /bin/menu /bin/MENU
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/hp3icc/Easy-FreeDMR-SERVER-Install/main/menu.sh)"
 
 sudo cat > /opt/obp.txt <<- "EOF"
 #Coloque abajo su lista de obp
