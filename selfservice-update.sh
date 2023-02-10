a94e1a5009c793e6faa6756cac08bd62ba7f7aa4
Esteban Mackay Q <49044505+hp3icc@users.noreply.github.com>
2023-02-10 14:12:59 -0500

    Update selfservice-update.sh

--- selfservice-update.sh
+++ selfservice-update.sh
@@ -183,8 +183,9 @@ sh /opt/FDMR-Monitor/sysinfo/rrd-db.sh
 
 cp -r /opt/FDMR-Monitor/sysinfo/ /var/fdmr/html/sysinfo/
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/hp3icc/Easy-FreeDMR-SERVER-Install/main/menu.sh)"
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/hp3icc/emq-TE1ws/main/self/data-id-update.sh)"
-data-id
+#sh -c "$(curl -fsSL https://raw.githubusercontent.com/hp3icc/emq-TE1ws/main/self/data-id-update.sh)"
+#data-id
+cronedit.sh '* */6 * * *' 'data-id' remove
 #sudo rm mon.db
 #sudo python3 mon_db.py
 sudo systemctl daemon-reload	
