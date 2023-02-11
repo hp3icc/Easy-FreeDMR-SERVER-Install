1a3344eeabcbf823160a677c147e09b832ed406e
Esteban Mackay Q <49044505+hp3icc@users.noreply.github.com>
2023-02-11 01:17:41 -0500

    Update selfservice-update.sh

--- selfservice-update.sh
+++ selfservice-update.sh
@@ -181,7 +181,7 @@ sh /opt/FDMR-Monitor/sysinfo/rrd-db.sh
 #sed '33 a <!--' -i /var/www/html/sysinfo.php
 #sed '35 a -->' -i /var/www/html/sysinfo.php
 
-cp -r /opt/FDMR-Monitor/sysinfo/ /var/fdmr/html/sysinfo/
+cp -r /opt/FDMR-Monitor/sysinfo/ /var/www/fdmr/sysinfo/
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/hp3icc/Easy-FreeDMR-SERVER-Install/main/menu.sh)"
 #sh -c "$(curl -fsSL https://raw.githubusercontent.com/hp3icc/emq-TE1ws/main/self/data-id-update.sh)"
 #data-id
